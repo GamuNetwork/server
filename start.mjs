513405712f091206b1bb28144d069b30d4a7e972
Mimifan <mimifan.dev@gmail.com>
2024-04-03 16:21:25 +0200

    Changes most files to module scripts. Updates Gitignore

--- start.mjs
+++ start.mjs
@@ -1,6 +1,16 @@
+// SERVER IMPORTS
+
+// DotEnv to read .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import ServerWebApp from './modules/webserverapp/main.mjs';
+import DiscordClient from './discordbot/main.mjs';
+import Logger from './modules/logger/main.mjs';
+
+const discordBot = new DiscordClient();
+discordBot.login(process.env.DISCORD_TOKEN);
 
 const server = new ServerWebApp();
-
-
 server.listen();
