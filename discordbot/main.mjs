2daa09b277f629a162c314fadbb0e19e4a6b6d9a
Mimifan <mimifan.dev@gmail.com>
2024-04-03 16:31:01 +0200

    Removes useless things atm in discordbot

--- discordbot/main.mjs
+++ discordbot/main.mjs
@@ -1,27 +1,6 @@
 import Discord from 'discord.js';
 import Logger from '../modules/logger/main.mjs';
 
-// import { REST, Routes } from 'discord.js';
-
-// const commands = [
-//   {
-//     name: 'ping',
-//     description: 'Replies with Pong!',
-//   },
-// ];
-
-// const rest = new REST({ version: '10' }).setToken("MTIyNDgxMDQ1NjQyMjU0NzU4Nw.GKO6rl.WRhOalP40bd6EDzPKsxUCljMS31HDb4v-HJb68");
-
-// try {
-//     console.log('Started refreshing application (/) commands.');
-  
-//     await rest.put(Routes.applicationCommands("1224810456422547587"), { body: commands });
-  
-//     console.log('Successfully reloaded application (/) commands.');
-//   } catch (error) {
-//     console.error(error);
-//   }
-
 export default class DiscordClient {
 
     static _instance = null;
@@ -38,14 +17,6 @@ export default class DiscordClient {
             });
 
             this.#client.on('ready', () => { this.onReady(); });
-            // this.client.on('interactionCreate', async interaction => {
-            //     if (!interaction.isChatInputCommand()) return;
-              
-            //     if (interaction.commandName === 'ping') {
-            //         const attachment = new AttachmentBuilder('./github_logo.png', {name: 'github_logo.png'});
-            //         await interaction.reply({files: [attachment]});
-            //     }
-            // });
 
         }
         return DiscordClient._instance;
@@ -56,19 +27,6 @@ export default class DiscordClient {
         console.log(`Discord bot logged in as ${this.#client.user.tag}!`);
         this._guild = await this.#client.guilds.fetch("1223995903342411796");
         this.logger = new Logger();
-    }
-    
-    onMessage(message) {
-        if (message.author.bot) return;
-        if (message.content === 'ping') {
-            message.reply('chut');
-            message.channel.send(new Discord.Attachment('/github_logo.png', 'github_logo.png'))
-            .then(msg => {
-                
-            })
-            .catch(console.error);
-            return;
-        }
     }
     
     login(TOKEN) { 
