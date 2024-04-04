d4d54f82f393941b71a61ea3cf59851b9dd79e92
Mimifan <mimifan.dev@gmail.com>
2024-04-04 18:19:37 +0200

    Adds comments on imports

--- start.mjs
+++ start.mjs
@@ -4,13 +4,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-import ServerWebApp from './modules/webserverapp/main.mjs';
+// Discord Bot to handle some archiving and further features
 import DiscordClient from './discordbot/main.mjs';
-import Logger from './modules/logger/main.mjs';
-
 const discordBot = new DiscordClient();
 discordBot.login(process.env.DISCORD_TOKEN);
+
+// ServerWebApp to handle the web server
+import ServerWebApp from './modules/webserverapp/main.mjs';
+
+// Logger to log the server activity
+import Logger from './modules/logger/main.mjs';
+
 
 const server = new ServerWebApp();
 server.listen();
