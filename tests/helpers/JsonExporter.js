fff4a65ded5e3e814873536f997f996dc26783a8
T0ine34 <91411073+T0ine34@users.noreply.github.com>
2024-04-17 21:09:37 +0200

    Adding tests exports and unit tests (#3)
    * Create main.yml
    
    * first steps with actions
    
    * adding autoUpdate script
    
    * adding actions and test reporter
    
    * updating actions run conditions
    
    * adding tests  results export
    
    * testing result upload
    
    * updating unitTest workflow path
    
    * updating gitignores
    
    * updating test export method
    
    * adding python env/ to gitignore
    
    * adding start script to package.json
    
    * updating export for tests according to the update of devtools/tests-exporter
    
    * adding a orphan test
    
    * updating ci with new report file
    
    * updating tests to be ran on 3 differents os
    
    * adding platform in test report
    
    * artifact name now contain the os
    
    * fixing report.json not having differents names on differents os
    
    * specs keys are now their ids

---
+++ tests/helpers/JsonExporter.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const packageJson = require('../../package.json');
+const { start } = require('repl');
+
+function sweetMerge(dict1, dict2){
+    for (var key in dict2){
+        if (dict1.hasOwnProperty(key)){
+            if (typeof dict1[key] === 'object' && typeof dict2[key] === 'object'){
+                dict1[key] = sweetMerge(dict1[key], dict2[key]);
+            } else {
+                dict1[key] = dict2[key];
+            }
+        }
+        else {
+            dict1[key] = dict2[key];
+        }
+    }
+    return dict1;
+}
+
+function getOS(){
+    switch(process.platform){
+        case 'win32':
+            return 'windows';
+        case 'darwin':
+            return 'macos';
+        case 'linux':
+            return 'ubuntu';
+        default:
+            return "Unknown OS"
+    }
+}
+
+const JsonExporter = function(outputFile){
+
+    var suites = {};
+    var orphans = {
+        passed: 0,
+        failed: 0,
+        pending: 0,
+        skipped: 0,
+        duration: 0,
+        specs: {}
+    };
+
+    var summary = {
+        appName: packageJson.name,
+        appVersion: packageJson.version,
+        os: getOS(),
+        specs: 0,
+        failures: 0,
+        passed: 0,
+        pending: 0,
+        skipped: 0,
+        duration: 0,
+        startDate: new Date().toISOString()
+    };
+
+    this.suiteStarted = function(suiteInfo) {
+        suites[suiteInfo.id] = sweetMerge(suiteInfo, {passed: 0, failed: 0, pending: 0, skipped: 0, duration: 0, specs: {}});
+    }
+
+    this.specDone = function(result) {
+        parentSuite = null;
+        if(result.parentSuiteId){
+            parentSuite = suites[result.parentSuiteId]
+            parentSuite.specs[result.id] = result;
+        }
+        else{
+            parentSuite = orphans;
+            orphans.specs[result.id] = result;
+        }
+
+        summary.specs++;
+        summary.duration += result.duration;
+        parentSuite.duration += result.duration;
+
+        if(result.pendingReason === 'Temporarily disabled with xit'){
+            summary.skipped++;
+            parentSuite.skipped++;
+        }
+        else if(result.status === 'failed'){
+            summary.failures++;
+            parentSuite.failed++;
+        }
+        else if(result.status === 'pending'){
+            summary.pending++;
+            parentSuite.pending++;
+        }
+        else{
+            summary.passed++;
+            parentSuite.passed++;
+        }
+    }
+    
+    this.suiteDone = function(result) {
+        let SuiteSpecs = {};
+        for (let spec in suites[result.id].specs){
+            SuiteSpecs[spec] = suites[result.id].specs[spec];
+        }
+        suites[result.id].specs = SuiteSpecs;
+        suites[result.id] =
+        sweetMerge(suites[result.id], result)
+    }
+
+    this.jasmineDone = function(suiteInfo) {
+        fs.writeFileSync(outputFile, JSON.stringify({summary: summary, suites: suites, orphans: orphans}, null, 4));
+    }
+};
+
+reporter = new JsonExporter("report."+getOS()+".json");
+
+jasmine.getEnv().addReporter(reporter);
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,5 +1,24 @@
 describe('Test', () => {
     it('should pass', () => {
         expect(true).toBe(true);
+        expect(1).toBe(1);
     });
+    it('should fail', () => {
+        expect(false).toBe(true);
+    });
+    it('should be pending', () => {
+        pending('this is a pending test');
+    });
+    xit('should be disabled', () => {
+        expect(true).toBe(true);
+    });
+    it('should fail', () => {
+        expect(true).toBe(true);
+        expect(false).toBe(false);
+        expect(1).toBe(2);
+    });
+});
+
+it('should be a top-level test', () => {
+    expect(true).toBe(true);
 });
