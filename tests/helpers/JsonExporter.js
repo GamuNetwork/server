f3b0c277023fead09d9eb6e1a08af6a0ba5a71ab
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-04-19 23:59:51 +0200

    jsonExporter now export file with errors inside json report

--- tests/helpers/JsonExporter.js
+++ tests/helpers/JsonExporter.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const packageJson = require('../../package.json');
 
+function int(value){
+    return parseInt(value, 10);
+}
+
 function sweetMerge(dict1, dict2){
     for (var key in dict2){
         if (dict1.hasOwnProperty(key)){
@@ -16,6 +20,46 @@ function sweetMerge(dict1, dict2){
     }
     return dict1;
 }
+
+function exportCodeFile(Filepath, lineNumber, contextLines = 5){
+    if(Filepath !== undefined && Filepath !== ""){
+        var file = fs.readFileSync(Filepath, 'utf8').split('\n');
+        var start = Math.max(0, lineNumber - contextLines);
+        var end = Math.min(file.length, int(lineNumber) + int(contextLines));
+        var lines = file.slice(start, end);
+        let linesWithNumbers = {};
+        for (var i = 0; i < lines.length; i++){
+            linesWithNumbers[start+i+1] = lines[i];
+        }
+        return linesWithNumbers;
+    }
+    return [];
+}
+
+function parseStack(stack){ //type: string
+    if(stack !== undefined && stack !== ""){
+        stack = stack.split("    at");
+
+        var newStack = [];
+
+        for (var i = 1; i < stack.length; i++){
+            var line = stack[i].replace(/\n/g, "")
+            //find if the line contain a file path
+            var filePath = line.match(/[A-Z]:\\.*\.[a-z]+:\d+:\d+/g);
+            if(filePath !== null){
+                let tokens = filePath[0].match(/([A-Z]:\\.*\.[a-z]+):(\d+):(\d+)/).slice(1);
+                newStack.push({
+                    filePath: tokens[0],
+                    lineNumber: tokens[1],
+                    columnNumber: tokens[2]
+                });
+            }
+        }
+        return newStack;
+    }
+    return [];
+}
+
 
 function getOS(){
     switch(process.platform){
@@ -42,6 +86,8 @@ const JsonExporter = function(outputFile){
         specs: {}
     };
 
+    var FilesToExport = {};
+
     var summary = {
         appName: packageJson.name,
         appVersion: packageJson.version,
@@ -61,28 +107,45 @@ const JsonExporter = function(outputFile){
 
     this.specDone = function(result) {
         parentSuite = null;
-        if(result.parentSuiteId){
-            parentSuite = suites[result.parentSuiteId]
-            parentSuite.specs[result.id] = result;
+
+        const cloneResult = structuredClone(result);
+
+        if(cloneResult.parentSuiteId){
+            parentSuite = suites[cloneResult.parentSuiteId]
         }
         else{
             parentSuite = orphans;
-            orphans.specs[result.id] = result;
+        }
+
+        parentSuite.specs[cloneResult.id] = cloneResult;
+
+        passedExpectations = parentSuite.specs[cloneResult.id].passedExpectations;
+        failedExpectations = parentSuite.specs[cloneResult.id].failedExpectations;
+        for (var i = 0; i < passedExpectations.length; i++){
+            passedExpectations[i].stack = parseStack(passedExpectations[i].stack);
+        }
+        for (var i = 0; i < failedExpectations.length; i++){
+            let stack = parseStack(failedExpectations[i].stack);
+            failedExpectations[i].stack = stack;
+            for (var j = 0; j < stack.length; j++){
+                let lines = exportCodeFile(stack[j].filePath, stack[j].lineNumber);
+                FilesToExport[stack[j].filePath+":"+stack[j].lineNumber] = lines;
+            }
         }
 
         summary.specs++;
-        summary.duration += result.duration;
-        parentSuite.duration += result.duration;
+        summary.duration += cloneResult.duration;
+        parentSuite.duration += cloneResult.duration;
 
-        if(result.pendingReason === 'Temporarily disabled with xit'){
+        if(cloneResult.pendingReason === 'Temporarily disabled with xit'){
             summary.skipped++;
             parentSuite.skipped++;
         }
-        else if(result.status === 'failed'){
+        else if(cloneResult.status === 'failed'){
             summary.failures++;
             parentSuite.failed++;
         }
-        else if(result.status === 'pending'){
+        else if(cloneResult.status === 'pending'){
             summary.pending++;
             parentSuite.pending++;
         }
@@ -103,7 +166,12 @@ const JsonExporter = function(outputFile){
     }
 
     this.jasmineDone = function(suiteInfo) {
-        fs.writeFileSync(outputFile, JSON.stringify({summary: summary, suites: suites, orphans: orphans}, null, 4));
+        fs.writeFileSync(outputFile, JSON.stringify({
+            summary: summary,
+            suites: suites,
+            orphans: orphans,
+            files: FilesToExport
+        }, null, 4));
     }
 };
 
