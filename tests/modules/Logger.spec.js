018caefc5df1e6c78cb2e252e7652250568ff423
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-05-08 15:32:45 +0200

    removing fake tests

--- tests/modules/Logger.spec.js
+++ tests/modules/Logger.spec.js
@@ -1,9 +1,1 @@
-describe('Testing Logger module', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
-    });
-
-    it('should log a message', () => {
-        expect(false).toBe(true);
-    });
-});
+// tests for logger module
