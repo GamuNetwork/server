94af4a70edb19b6cb9712fb4d497aa614205fcd7
T0ine34 <91411073+T0ine34@users.noreply.github.com>
2024-05-08 17:32:01 +0200

    Removing useless tests (#5)
    * removing fake tests
    
    * adding basic useless test for logger

--- tests/modules/Logger.spec.js
+++ tests/modules/Logger.spec.js
@@ -2,8 +2,4 @@ describe('Testing Logger module', () => {
     it('should pass', () => {
         expect(true).toBe(true);
     });
-
-    it('should log a message', () => {
-        expect(false).toBe(true);
-    });
 });
