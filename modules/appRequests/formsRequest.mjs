0e0eedac4d722ba8b3145d0097ce8701f94a2ddf
Mimifan <mimifan.dev@gmail.com>
2024-07-04 12:26:17 +0200

    Adds Form Requests handle

---
+++ modules/appRequests/formsRequest.mjs
@@ -0,0 +1,11 @@
+export function handleRedirectRegister(req, res) {
+    res.send('Register');
+}
+
+export function handleRedirectLogin(req, res) {
+    res.send('Login');
+}
+
+export function handleRedirectForgotPassword(req, res) {
+    res.send('Forgot Password');
+}
